36741637a05419bd25f13597272d719628841042
yndgow <yndgow@gmail.com>
2022-08-23 22:56:45 +0900

    10818 solve

--- Backjoon/src/step04/_01.java
+++ Backjoon/src/step04/_01.java
@@ -30,45 +30,33 @@ public class _01 {
 		str = br.readLine();
 		st =  new StringTokenizer(str, " ");
 		
-		if(nCount != 1) {
-			
-			// 입력 받은 숫자 배열 저장
-			for(int i=0; i<nCount; i++) {
-				nNum = Integer.parseInt(st.nextToken());
-				array[i] = nNum;
-			}
-			
-			// 최소값
-			int min = array[0];
-			for(int k=1; k<nCount; k++) {
-				if(min > array[k]) {
-					min = array[k];
-				}
-			}
-			
-			//최소값 입력
-			bw.write(min+" ");
-
-			// 최대값
-			int max = array[0]; 
-			for(int j=1; j<nCount; j++) {
-				if(max < array[j]) {
-					max = array[j];
-				}
-			}
-			
-			// 최대값 입력
-			bw.write(max+"");
-			
-			br.close();
-			bw.flush();
-			bw.close();
-
-		}else {
-			bw.write(str + " " + str);
-			br.close();
-			bw.flush();
-			bw.close();	
+					// 입력 받은 숫자 배열 저장
+		for(int i=0; i<nCount; i++) {
+			nNum = Integer.parseInt(st.nextToken());
+			array[i] = nNum;
 		}
+		
+		// 최소값
+		int min = array[0];
+		for(int k=1; k<nCount; k++) {
+			if(min > array[k]) {
+				min = array[k];
+			}
+		}
+
+		// 최대값
+		int max = array[0]; 
+		for(int j=1; j<nCount; j++) {
+			if(max < array[j]) {
+				max = array[j];
+			}
+		}
+		
+		// 최대값 입력
+		bw.write(min + " " +max);
+		
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
