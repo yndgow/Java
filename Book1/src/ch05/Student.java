c057b1adba26db60469f2ae2e12ed9a68e17fbd3
yndgow <yndgow@gmail.com>
2022-08-23 13:50:05 +0900

    20220823off

---
+++ Book1/src/ch05/Student.java
@@ -0,0 +1,12 @@
+package ch05;
+
+public class Student {
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address); 
+	}
+}
