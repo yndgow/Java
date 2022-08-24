7106ba2523cde5a87c7d0cebdf2194b6a547bb39
yndgow <yndgow@gmail.com>
2022-08-25 08:53:02 +0900

    정리

--- Backjoon/src/step07/q2557.java
+++ Backjoon/src/step07/q2557.java
@@ -3,7 +3,6 @@ package step07;
 public class q2557 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- Backjoon/src/step09/q2557.java
+++ Backjoon/src/step09/q2557.java
@@ -3,7 +3,6 @@ package step09;
 public class q2557 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
