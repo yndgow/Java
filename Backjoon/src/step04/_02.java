f9e06bcdbb8624a10517983575dd2efb319c8f0e
yndgow <yndgow@gmail.com>
2022-08-24 13:45:18 +0900

    Backjoon 2562

--- Backjoon/src/step04/_02.java
+++ Backjoon/src/step04/_02.java
@@ -1,5 +1,12 @@
 package step04;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
 /*
  * 날짜 : 2022/08/23
  * 이름 : 김지홍
@@ -9,10 +16,29 @@ package step04;
 
 public class _02 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		Integer [] arr = new Integer [9];
+		for(int i=0; i<9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		int max = arr[0];
+		for(int j=1; j<arr.length; j++) {
+			if(arr[j] > max) {
+				max = arr[j];
+			}
+		}
+		int index = Arrays.asList(arr).indexOf(max);
 		
+		bw.write(max+"");
+		bw.newLine();
+		bw.write(index+"");
 		
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 
 }
