e47d8fd04260cd77cdc444d61d47001df8cb894d
yndgow <yndgow@gmail.com>
2022-08-29 10:25:56 +0900

    Java 상속실습 예제

---
+++ Ch05/src/sub5/Car.java
@@ -0,0 +1,55 @@
+package sub5;
+
+// 클래스 정의
+public class Car {
+	
+	// 자동차의 속성(변수)
+	protected String name;
+	protected String color;
+	protected int speed;
+	
+	// 생성자
+	public Car(String name, String color, int speed) {
+		this.name = name;
+		this.color = color;
+		this.speed = speed;
+	}
+	
+	// Getter, Setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	
+	// 자동차의 기능(메서드)
+	public void speedUp(int speed) {
+		this.speed += speed;
+	}
+	
+	public void speedDown(int speed) {
+		this.speed -= speed;
+	}
+	
+	public void show() {
+		System.out.println("차량명 : " + this.name);
+		System.out.println("차량색 : " + this.color);
+		System.out.println("속도 : " + this.speed);
+	}
+}
