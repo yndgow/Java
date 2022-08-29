821abeb46db442a3b56b5a0794d400742b15e98d
yndgow <yndgow@gmail.com>
2022-08-30 00:09:05 +0900

    book 0603 static 변수

---
+++ Book1/src/ch06_3/Student.java
@@ -0,0 +1,17 @@
+package ch06_3;
+
+public class Student {
+	public static int serialNum = 1000;
+	public int studentID;
+	public String studentName;
+	public int grade;
+	public String address;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String name) {
+		studentName = name;
+	}
+}
---
+++ Book1/src/ch06_3/StudentTest5.java
@@ -0,0 +1,7 @@
+package ch06_3;
+
+public class StudentTest5 {
+	public static void main(String[] args) {
+		System.out.println(Student2.getSerialNum());
+	}
+}
