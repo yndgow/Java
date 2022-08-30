b6e0f497f6dcfc05ae6d2b1f6f4139a03654ac23
yndgow <yndgow@gmail.com>
2022-08-30 20:01:04 +0900

    2022/08/30 baekjoon OX 퀴즈 8958

--- Backjoon/src/step04/_05.java
+++ Backjoon/src/step04/_05.java
@@ -16,25 +16,34 @@ public class _05 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		// 횟수 입력
 		int count = 0;
 		count = Integer.parseInt(br.readLine());
 		
+		// OX 입력
 		String result;
-		
 		for(int i=0; i<count; i++) {
 			result = br.readLine();
-			String [] arr = result.split("");
+			String [] arr = result.split(""); // OX 잘라서 배열 넣기
+
+			// O 찾기
 			int score = 0;	
-			for(int j=0; j<3; j++) {
-				if(arr[j].equals("O")) {
+			for(int j=0; j<arr.length; j++) { // 입력한 배열길이 만큼 반복
+				if(arr[j].equals("O")) { // O 와 같다면 1점 추가
 					score++;
-					if( j>0 && arr[j-1].equals("O") && arr[j].equals("O")) {
+					int k = 1;
+					// 배열 앞의 값과 같다면 1추가, 그전과 또 같다면 1추가
+					while(j>=k && arr[j-k].equals("O")) {  
 						score++;
+						k++;
 					}
 				}
-				System.out.print(score);
 			}
-			System.out.println(score);
+			bw.write(score+"");
+			bw.newLine();
 		}
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
