04c027466dd165a7ea8f8eb904a08f5c90cb8efe
yndgow <yndgow@gmail.com>
2022-09-01 13:30:20 +0900

    상속, 오버라이드, 가상메서드

--- Book1/src/ch08/Customer.java
+++ Book1/src/ch08/Customer.java
@@ -10,7 +10,7 @@ public class Customer {
 	public Customer() {
 		customerGrade = "SILVER";
 		bonusRatio = 0.01;
-		System.out.println("Customer() 생성자 호출");
+//		System.out.println("Customer() 생성자 호출");
 	}
 	
 	public Customer(int customerID, String customerName) {
@@ -18,7 +18,7 @@ public class Customer {
 		this.customerName = customerName;
 		customerGrade = "SILVER";
 		bonusRatio = 0.01;
-		System.out.println("Customer(int, String) 생성자 호출");
+//		System.out.println("Customer(int, String) 생성자 호출");
 	}
 	
 	public int getCustomerID() {
