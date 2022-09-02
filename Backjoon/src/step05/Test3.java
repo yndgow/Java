19fdd64e3096692c15d2084c8f62984d6600f66c
yndgow <yndgow@gmail.com>
2022-09-02 13:54:36 +0900

    1065 ing

--- Backjoon/src/step05/Test3.java
+++ Backjoon/src/step05/Test3.java
@@ -9,28 +9,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Test3 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(br.readLine());
-		hansu(num);
+		int num = Integer.parseInt(br.readLine()); // 숫자입력
+		int result = 0; // 결과값 저장
+		
+
+		// 입력된 숫자까지 반복문
+		for(int i=1; i<=num; i++) {
+			if(hansu(i)==true) {
+				result++; // 한수라면 ++
+			}
+			
+		}
+		// result 출력
+		System.out.println(result);
 	}
-	static int hansu(int num) {
+	
+	static boolean hansu(int num) {
+		if(num<100) return true; // 100미만 true;
 		
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		
-		arr.add(num % 10);
-		
-		num /= 10;
-		
-		if(num != 0) {
-			return hansu(num);
-		}else {
-			arr.size();
-			arr.get(arr.size()-1);
-			return 0;
+		for (int j = 0; j<4; j++) {
+			arr.add(num % 10);
+			num /= 10;
+			if(num == 0) break;
 		}
-		
+		if (arr.get(0) <= arr.get(1)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
