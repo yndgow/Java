fdb456f8a0190509ef540e7ed6bae293b8155e25
yndgow <yndgow@gmail.com>
2022-09-05 13:24:05 +0900

    2022/09/05 baekjoon 한수 1065

--- Backjoon/src/step05/Test3.java
+++ Backjoon/src/step05/Test3.java
@@ -1,6 +1,6 @@
 package step05;
 /*
- * 날짜 : 2022/09/01
+ * 날짜 : 2022/09/01 ~ 05
  * 이름 : 김지홍
  * 내용 : 한수 1065
  */
@@ -8,8 +8,6 @@ package step05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Test3 {
 	public static void main(String[] args) throws IOException {
@@ -17,14 +15,11 @@ public class Test3 {
 		int num = Integer.parseInt(br.readLine()); // 숫자입력
 		int result = 0; // 결과값 저장
 		
-		if(num>=100) result = 99;
 		// 입력된 숫자까지 반복문
 		for(int i=1; i<=num; i++) {
 			if(hansu(i)==true) {
-				
 				result++; // 한수라면 ++
 			}
-			
 		}
 		// result 출력
 		System.out.println(result);
@@ -34,13 +29,14 @@ public class Test3 {
 		if(num<100) {
 			return true; // 100미만 true;
 		}else if(num >=100) {
-			int num1 = 0;
-			int num2 = 0;
-			int num3 = 0;
+			int num1 = num % 10; // 1의자리
+			int num2 = (num / 10) % 10; //10의자리
+			int num3 = num / 100; // 100의자리
 			
-			
+			if(num3 - num2 == num2 - num1 ) { // 등차수열확인
+				return true;
+			}
 		}
 		return false;
-		
 	}
 }
