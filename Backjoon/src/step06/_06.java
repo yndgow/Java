cec458a43eecfe61ede9160c4503f978cfe3ba73
yndgow <yndgow@gmail.com>
2022-09-07 13:52:07 +0900

    test

---
+++ Backjoon/src/step06/_06.java
@@ -0,0 +1,11 @@
+package step06;
+
+public class _06 {
+
+	public static void main(String[] args) {
+		String i1 = "1";
+		Integer i2 = Integer.valueOf(i1);
+		System.out.println(i2.equals(1));
+		
+	}
+}
