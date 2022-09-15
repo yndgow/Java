53edaeaa49b574785c7db226227b3410b8165969
yndgow <yndgow@gmail.com>
2022-09-15 13:20:58 +0900

    2022/09/15 btest

--- Backjoon/src/step06/_06.java
+++ Backjoon/src/step06/_06.java
@@ -3,9 +3,9 @@ package step06;
 public class _06 {
 
 	public static void main(String[] args) {
-		String i1 = "1";
-		Integer i2 = Integer.valueOf(i1);
-		System.out.println(i2.equals(1));
+		char m = 'z';
+		int M = (int)m;
+		System.out.println(M);
 		
 	}
 }
