27b6abffecb05dcc86a0ab2cf6dfd1fadc7ef1e5
yndgow <yndgow@gmail.com>
2022-09-18 23:13:00 +0900

    2022/09/18 Book 12-3 p422

---
+++ Book1/src/ch12/Point.java
@@ -0,0 +1,21 @@
+package ch12;
+
+public class Point<T, V> {
+	T x;
+	V y;
+	
+	public Point(T x, V y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public T getX() {
+		return x;
+	}
+
+	public V getY() {
+		return y;
+	}
+	
+	
+}
---
+++ Book1/src/ch12collection/arraylist/MemberArrayTest.java
@@ -0,0 +1,27 @@
+package ch12collection.arraylist;
+
+import ch12collection.Member;
+
+public class MemberArrayTest {
+	public static void main(String[] args) {
+		MemberArrayList memberArrayList = new MemberArrayList();
+		Member memberLee 	= new Member(1001, "이지원");
+		Member memberSon 	= new Member(1002, "손민국");
+		Member memberPark 	= new Member(1003, "박서훤");
+		Member memberHong 	= new Member(1004, "홍길동");
+		
+		memberArrayList.addMember(memberLee);
+		memberArrayList.addMember(memberSon);
+		memberArrayList.addMember(memberPark);
+		memberArrayList.addMember(memberHong);
+		
+		memberArrayList.showAllMember();
+		
+		memberArrayList.removeMember(memberHong.getMemberId());
+		memberArrayList.showAllMember();
+		
+		memberArrayList.insertMember(memberHong, 1);
+		memberArrayList.showAllMember();
+		
+	}
+}
