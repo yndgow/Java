07e48d11fffffede69042466a90bce0b4c707c39
yndgow <yndgow@gmail.com>
2022-09-19 13:44:44 +0900

    2022/09/19 백준 단어공부 1157

--- Backjoon/src/step06/_05.java
+++ Backjoon/src/step06/_05.java
@@ -1,31 +1,54 @@
 package step06;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /*
- * 날짜 : 2022/09/06
+ * 날짜 : 2022/09/06 ~ 19
  * 이름 : 김지홍
  * 내용 : 단어 공부 1157
  */
 
 public class _05 {
-	public static void main(String[] args) {
-		String a1 = "Mississipi";
-		String a2 = a1.toUpperCase();
-		ArrayList<Integer> list = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine().toUpperCase(); // 입력 문자 모두 대문자화
+		int [] arr = new int [26]; // A~Z 까지 반복횟수를 담을 배열
 		
-		for(int i=0; i<a1.length(); i++) {
-			int n = 0;
-			for(int j=0; j<a1.length(); j++) {
-				if(a2.charAt(i)  == a2.charAt(j)) {
-					n++;
+		// 각 문자의 반복횟수 만큼 해당 배열값 증가
+		for(int i=0; i<input.length(); i++) {
+			char c = input.charAt(i);
+			arr[c-65]++;
+		}
+		
+		// 가장 많이 사용된 값 찾기
+		int max = 0;
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+
+		// max 중복 검사
+		int count = 0;
+		for(int i=0; i<arr.length; i++) {
+			if(max == arr[i]) {
+				count++;
+			}
+		}
+
+		// count 에 따라 결과출력
+		if(count > 1) {
+			System.out.println("?");
+		}else {
+			for(int i=0; i<arr.length; i++) {
+				if(max == arr[i]) {
+					System.out.println((char)(i+65));
 				}
 			}
-			list.add(i, n);
 		}
 		
-		for (Integer i : list) {
-			System.out.print(i);
-		}
+		br.close();
 	}
 }
