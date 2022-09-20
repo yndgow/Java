c79be7d344e7700dd3f0776992a88f818fe6fe77
yndgow <yndgow@gmail.com>
2022-09-21 00:41:12 +0900

    2022/09/21 Book TreeSet Comapare

--- Book1/src/ch12collection/Member2.java
+++ Book1/src/ch12collection/Member2.java
@@ -1,11 +1,13 @@
 package ch12collection;
 
-public class Member implements Comparable<Member>{
+import java.util.Comparator;
+
+public class Member2 implements Comparator<Member2>{
 	
 	private int memberId;
 	private String memberName;
 
-	public Member(int memberId, String memberName) {
+	public Member2(int memberId, String memberName) {
 		this.memberId = memberId;
 		this.memberName = memberName;
 	}
@@ -38,8 +40,8 @@ public class Member implements Comparable<Member>{
 	
 	@Override
 	public boolean equals(Object obj) {
-		if(obj instanceof Member) {
-			Member member = (Member)obj;
+		if(obj instanceof Member2) {
+			Member2 member = (Member2)obj;
 			if(this.memberId == member.memberId)
 				return true;
 			else
@@ -49,8 +51,10 @@ public class Member implements Comparable<Member>{
 	}
 
 	@Override
-	public int compareTo(Member member) {
-		return this.memberName.compareTo(member.memberName);
+	public int compare(Member2 mem1, Member2 mem2) {
+		return mem1.getMemberId() - mem2.getMemberId();
 	}
+
+
 	
 }
