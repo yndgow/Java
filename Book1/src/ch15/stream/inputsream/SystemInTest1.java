69ee25dd3ba64b059d4b8324e13cb720fd09ac65
yndgow <yndgow@gmail.com>
2022-09-28 12:51:37 +0900

    2022/09/28 Book 스트림

---
+++ Book1/src/ch15/stream/inputsream/SystemInTest1.java
@@ -0,0 +1,19 @@
+package ch15.stream.inputsream;
+
+import java.io.IOException;
+
+public class SystemInTest1 {
+	public static void main(String[] args) {
+		System.out.println("알파벳 여러 개를 쓰고 [Enter]를 누르세요");
+		
+		int i;
+		try {
+			while((i = System.in.read()) != -1) {			
+				System.out.print((char)i);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
