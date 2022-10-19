9809c3f9e76de05aaf5316c3b4ce466e374e06f8
yndgow <yndgow@gmail.com>
2022-10-19 13:53:57 +0900

    2022/10/19

---
+++ Backjoon/src/step07/_03.java
@@ -0,0 +1,32 @@
+package step07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 날짜 : 2022/10//07
+ * 이름 : 김지홍
+ * 내용 : 분수찾기 1193
+ * 		x가 주어졌을때, x번째를 구하는 프로그램
+ */
+
+public class _03 {
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			int x = Integer.parseInt(br.readLine());
+			
+			
+			
+			
+			
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
---
+++ Backjoon/src/step07/_04.java
@@ -0,0 +1,28 @@
+package step07;
+
+/*
+ * 날짜 : 2022/10/19
+ * 이름 : 김지홍
+ * 내용 : 달팽이는 올라가고 싶다 2869
+ * 		
+ */
+
+public class _04 {
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 1;
+		int t = 5;
+		
+		int d = a - b;
+		
+		int i = 1;
+		
+		while(true) {
+			
+		
+		}
+		
+		
+	}
+}
